f1e3597c3fabe56939f211d37bf619827c00837f
Lenny <lennygomes.hp@gmail.com>
2026-03-19 14:40:55 +0100

    SUPPRESSION MAILER

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { InsightModule } from './insight/insight.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { LucidModule } from './lucid/lucid.module';
-import { MailerService } from './mailer/mailer.service';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { RevenuecatModule } from './revenuecat/revenuecat.module';
 import { NotificationsModule } from './notifications/notifications.module';
@@ -17,6 +16,6 @@ import { NotificationsModule } from './notifications/notifications.module';
 @Module({
   imports: [ThrottlerModule.forRoot([{ ttl: 60000, limit: 50 }]) , ConfigModule.forRoot({ isGlobal: true }), ScheduleModule.forRoot(), AiModule, AuthModule, DreamsModule, CloudinaryModule, InsightModule, LucidModule, RevenuecatModule, NotificationsModule],
   controllers: [AppController],
-  providers: [AppService, MailerService],
+  providers: [AppService],
 })
 export class AppModule {}
